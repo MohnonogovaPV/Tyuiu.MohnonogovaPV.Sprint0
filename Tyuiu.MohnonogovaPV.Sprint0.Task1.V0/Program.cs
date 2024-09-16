e1259794ec8f13da40f9ce72f786b1ba1a475aec
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-09-16 23:40:24 +0500

    Task1

---
+++ Tyuiu.MohnonogovaPV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.MohnonogovaPV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Полина";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
