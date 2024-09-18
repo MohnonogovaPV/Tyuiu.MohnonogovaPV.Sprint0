f6d0151a6a6445221a131a4f193ae1a7f67c4393
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-09-18 21:45:35 +0500

    Task3

---
+++ Tyuiu.MohnonogovaPV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tuyiu.MohnonogovaPV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.MohnonogovaPV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tuyiu.MohnonogovaPV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
