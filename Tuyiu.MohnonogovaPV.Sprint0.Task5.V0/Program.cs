3d1112c9b146d48fddaf21da91ce215b14bb9e10
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-09-18 23:15:40 +0500

    Task5

---
+++ Tuyiu.MohnonogovaPV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tuyiu.MohnonogovaPV.Sprint0.Task5.V0.Lib;
+namespace Tuyiu.MohnonogovaPV.Sprint0.Task5.V0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
